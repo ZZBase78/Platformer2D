961b27458c417c80c04413078ce6130b2d9ad3e7
ZZBase78 <88794134+ZZBase78@users.noreply.github.com>
2022-04-01 01:54:30 +0300

    Добавлен базовый класс для внесения кадров анимации

---
+++ Assets/AnimationScripts/AnimationTrack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer2D.Assets.AnimationScripts
+{
+    [CreateAssetMenu(fileName = "AnimationTrack", menuName = "ScriptableObjects/Animation/AnimationTrack", order = 1)]
+    internal sealed class AnimationTrack : ScriptableObject
+    {
+        public List<Sprite> list;
+    }
+}

bb51ada9f7d900cae33f1101fd6c7b910925e2e9
ZZBase78 <88794134+ZZBase78@users.noreply.github.com>
2022-04-05 18:35:05 +0300

    Сделан префаб пушки

---
+++ Assets/Enemy/Cannon/CannonView.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Platformer2D.Assets.Enemy.Cannon
+{
+    internal sealed class CannonView : MonoBehaviour
+    {
+        public Transform transformView;
+        public Transform rotatePoint;
+        public Transform fireStartPoint;
+    }
+}
